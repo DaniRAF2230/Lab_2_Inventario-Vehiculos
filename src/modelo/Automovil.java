0113049c715007c835ded2d7679ab482b7127356
DaniRAF <drsoto30@gmail.com>
2026-09-15 09:32:42 -0500

    Se crea el paquete Modelo el cual contiene las clases para los vehículos

---
+++ src/modelo/Automovil.java
@@ -0,0 +1,19 @@
+package modelo;
+
+
+    public class Automovil extends Vehiculo {
+        private final int cantidadPuertas;
+
+        public Automovil(String marca, String modelo, int anio,
+                         double precio, int cantidadPuertas) {
+            super(marca, modelo, anio, precio);
+            this.cantidadPuertas = cantidadPuertas;
+        }
+
+        @Override
+        protected String getCaracteristica() {
+            return "Puertas: " + cantidadPuertas;
+        }
+    }
+}
+
